a5684038a2a6d1e95b7b4092f1a8bc84d8fc5507
Alejandra <amonzon1113@gmail,com>
2022-10-30 00:47:31 -0500

    Casi conectado

--- SRI/Interface.cs
+++ SRI/Interface.cs
@@ -9,7 +9,7 @@ public interface ISearchResult : IEnumerable<SearchItem>
 {
     SearchItem this[int index] { get; }
 
-    /// <summary>
+    /// <summary>a
     /// Es la palabra más cercana a la consulta realizada
     /// </summary>
     string Suggestion { get; }
