5447bbd70b1dffe1f6e890a9f2ce5e03d16e89be
leo00010011 <ulloaferrerleonardo@gmail.com>
2022-11-02 23:32:55 -0400

    TestResolvingConflicts

--- tester/Program.cs
+++ tester/Program.cs
@@ -1,17 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
-using DP.Classes;
 
-
-var doc = new ProcesedDocument("casa casa pronombres perro gato");
-foreach(var item in doc)
-{
-    Console.WriteLine(item.Item1 + ": " + item.Item2.ToString());
-}
-=======
 Console.WriteLine("Hello, World!");
-
-Directory.GetFiles("URL", ".txt", new EnumerationOptions() { RecurseSubdirectories = true });
->>>>>>> 953eb19045f22e7be332208ce80704e42f2f0e95
 
 Directory.GetFiles("URL", ".txt", new EnumerationOptions() { RecurseSubdirectories = true });
