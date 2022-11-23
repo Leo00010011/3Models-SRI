92aa09821bf33b41864e7dfb1470c2733735ba89
Alejandra <amonzon1113@gmail,com>
2022-11-23 06:48:33 -0500

    Reuter and Cran Parser done

--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -74,27 +74,61 @@ public static class Parser
 {
     public static ParsedInfo NewsgroupParser(IEnumerable<char> file)
     {
-        int ti=0;
-        int tl=0;
         int count = 0;
-        int title_match_count=0;
         char[] matching_machine = new char[]{'s', 'u','b','j','e','c','t',':',' '};
         IEnumerator<char> file_enumerator = file.GetEnumerator();
-
-        while(file_enumerator.MoveNext())
-        {   
-            if(title_match_count >= matching_machine.Length)
-            {
-                ti = count++;
-                break;
-            }
-            if(file_enumerator.Current==matching_machine[title_match_count]) title_match_count++;
-            else title_match_count=0;
-            count++;
-        } 
+        int ti = Parser.MatchIndex(file_enumerator, matching_machine,out count,count);
         while(file_enumerator.MoveNext() && file_enumerator.Current != '\n') count++;
-        tl = (count++)-ti;
+        int  tl = (count++)-ti;
         int si = ti+tl +2;
         return new ParsedInfo(si,-1,ti,tl,ti);
     }
+
+    public static ParsedInfo CranParser(IEnumerable<char> file)
+    {
+        int count = 0;
+        char[] matching_title = new char[]{'.', 't','\n'};
+        char[] matching_end_title = new char[]{'.', 'a','\n'};
+        char[] matching_text = new char[]{'.', 'w','\n'};
+        IEnumerator<char> file_enumerator = file.GetEnumerator();
+        int ti= Parser.MatchIndex(file_enumerator,matching_title,out count,count);
+        int tl= Parser.MatchIndex(file_enumerator,matching_end_title,out count,count) - ti -3;
+        int texi = Parser.MatchIndex(file_enumerator,matching_text,out count,count);
+        return new ParsedInfo(texi, -1, ti, tl, texi);
+
+    }
+    
+    public static ParsedInfo ReutersParser(IEnumerable<char> file)
+    {
+        int count = 0;
+        char[] matching_title = new char[]{'<', 't','i','t','l','e','>'};
+        char[] matching_end_title = new char[]{'<','/', 't','i','t','l','e','>'};
+        char[] matching_text = new char[]{'<', 'b','o','d','y','>'};
+        IEnumerator<char> file_enumerator = file.GetEnumerator();
+        int ti= Parser.MatchIndex(file_enumerator,matching_title,out count,count);
+        int tl= Parser.MatchIndex(file_enumerator,matching_end_title,out count,count) - ti -8;
+        int texi = Parser.MatchIndex(file_enumerator,matching_text,out count,count);
+        return new ParsedInfo(texi, -1, ti, tl, texi);
+
+    }
+
+    private static int MatchIndex(IEnumerator<char> file_enumerator, char[] matching,out int current_index, int initial)
+    {
+        current_index = initial;
+        int match_count = 0;
+        int index_result = 0;
+        while(file_enumerator.MoveNext())
+        {   
+            if(match_count >= matching.Length)
+            {
+                index_result = current_index++;
+                break;
+            }
+            if(file_enumerator.Current==matching[match_count]) match_count++;
+            else match_count=0;
+            current_index++;
+        }
+        return index_result;
+    }
+
 }
--- tester/Program.cs
+++ tester/Program.cs
@@ -7,106 +7,125 @@ using SRI;
 using SRI.Interface;
 using Utils;
 
-namespace Test;
-public static class TestingMethods
-{
+#region FullFlowTest
+// namespace Test;
+// public static class TestingMethods
+// {
 
-}
+// }
 
-public class Program
-{
-    public static void Print(IEnumerable<char> text)
-    {
-        foreach (char c in text)
-        {
-            if (c == '\n')
-            {
-                Console.WriteLine();
-            }
-            else
-            {
-                Console.Write(c);
-            }
-        }
-    }
+// public class Program
+// {
+//     public static void Print(IEnumerable<char> text)
+//     {
+//         foreach (char c in text)
+//         {
+//             if (c == '\n')
+//             {
+//                 Console.WriteLine();
+//             }
+//             else
+//             {
+//                 Console.Write(c);
+//             }
+//         }
+//     }
 
-    public static IEnumerable<string> ReadAllFiles(string path)
-    {
-        foreach (var item in Directory.EnumerateFiles(path))
-        {
-            yield return item;
-        }
-        foreach (var docs in Directory.EnumerateDirectories(path))
-        {
-            foreach (var item in ReadAllFiles(docs))
-            {
-                yield return item;
-            }
-        }
-    }
+//     public static IEnumerable<string> ReadAllFiles(string path)
+//     {
+//         foreach (var item in Directory.EnumerateFiles(path))
+//         {
+//             yield return item;
+//         }
+//         foreach (var docs in Directory.EnumerateDirectories(path))
+//         {
+//             foreach (var item in ReadAllFiles(docs))
+//             {
+//                 yield return item;
+//             }
+//         }
+//     }
 
-    public static void Main(string[] args)
-    {
-        Stopwatch cloc = new Stopwatch();
-        IEnumerable<string> docsID = ReadAllFiles(".\\contents\\20 Newsgroups\\20news-18828");
+//     public static void Main(string[] args)
+//     {
+//         Stopwatch cloc = new Stopwatch();
+//         IEnumerable<string> docsID = ReadAllFiles(".\\contents\\20 Newsgroups\\20news-18828");
 
-        LinkedList<IDocument> docs = new LinkedList<IDocument>();
-        foreach (var item in docsID)
-        {
-           docs.AddLast(new Document(item, Parser.NewsgroupParser));
-        }
+//         LinkedList<IDocument> docs = new LinkedList<IDocument>();
+//         foreach (var item in docsID)
+//         {
+//            docs.AddLast(new Document(item, Parser.NewsgroupParser));
+//         }
 
-        cloc.Start();
-        ReadTest(docs);
-        cloc.Stop();
+//         cloc.Start();
+//         ReadTest(docs);
+//         cloc.Stop();
 
-        Console.WriteLine($"leer documentos solamente cuesta: {cloc.Elapsed}");
-        cloc.Reset();
+//         Console.WriteLine($"leer documentos solamente cuesta: {cloc.Elapsed}");
+//         cloc.Reset();
 
-        cloc.Start();
-        VSM vectorial = new VSM(docs);
-        cloc.Stop();
+//         cloc.Start();
+//         VSM vectorial = new VSM(docs);
+//         cloc.Stop();
 
-        Console.WriteLine($"construir el modelo cuesta: {cloc.Elapsed}");
-        cloc.Reset();
+//         Console.WriteLine($"construir el modelo cuesta: {cloc.Elapsed}");
+//         cloc.Reset();
 
-        SRIVectorDic<string, IWeight> query = new SRIVectorDic<string, IWeight>();
+//         SRIVectorDic<string, IWeight> query = new SRIVectorDic<string, IWeight>();
 
-        QueryVSMWeight hoW = new QueryVSMWeight(2, 2);
-        query.Add("house", hoW);
+//         QueryVSMWeight hoW = new QueryVSMWeight(2, 2);
+//         query.Add("house", hoW);
 
-        cloc.Start();
-        SearchItem[] results = vectorial.Ranking(vectorial.GetSearchItems(query, 30));
-        cloc.Stop();
+//         cloc.Start();
+//         SearchItem[] results = vectorial.Ranking(vectorial.GetSearchItems(query, 30));
+//         cloc.Stop();
 
-        Console.WriteLine($"buscar en el modelo cuesta: {cloc.Elapsed}");
+//         Console.WriteLine($"buscar en el modelo cuesta: {cloc.Elapsed}");
 
-        foreach (var item in results.Select(x => x.Title))
-        {
-            System.Console.WriteLine(SearchItem.Convert(item));
-        }
-    }
+//         foreach (var item in results.Select(x => x.Title))
+//         {
+//             System.Console.WriteLine(SearchItem.Convert(item));
+//         }
+//     }
 
-    private static char ReadTest(IEnumerable<IDocument> docs)
-    {
-        char a = '0';
-        foreach (var doc in docs)
-        {
-            foreach (var item in doc)
-            {
-                a = item;
-            }
-        }
-        return a;
-    }
-}
+//     private static char ReadTest(IEnumerable<IDocument> docs)
+//     {
+//         char a = '0';
+//         foreach (var doc in docs)
+//         {
+//             foreach (var item in doc)
+//             {
+//                 a = item;
+//             }
+//         }
+//         return a;
+//     }
+// }
+#endregion
 
+string s ="From: hgomez@magnus.acs.ohio-state.edu (Humberto L Gomez)\nsubject: MULTISYNC 3D NEC MONITOR FOR SALE\n\n\nI have an NEC multisync 3d monitor for sale. great condition. looks new. it is\n.28 dot pitch\nSVGA monitor that syncs from 15-38khz\n\nit is compatible with all aga amiga graphics modes.\nleave message if interested. make an offer.\n-- ";
+ParsedInfo a = Parser.NewsgroupParser(s);
 
-// string s ="From: hgomez@magnus.acs.ohio-state.edu (Humberto L Gomez)\nSubject: MULTISYNC 3D NEC MONITOR FOR SALE\n\n\nI have an NEC multisync 3d monitor for sale. great condition. looks new. it is\n.28 dot pitch\nSVGA monitor that syncs from 15-38khz\n\nit is compatible with all aga amiga graphics modes.\nleave message if interested. make an offer.\n-- ";
-// ParsedInfo a = Parser.NewsgroupParser(doc);
+System.Console.WriteLine("-----Newsgroup-----");
+System.Console.WriteLine($"Title init: {a.TitleInit}");
+System.Console.WriteLine($"Title Len: {a.TitleLen}");
+System.Console.WriteLine($"Text Init: {a.TextInit}");
+System.Console.WriteLine($"Snippet init: {a.SnippetInit}");
 
+s= "<title>BAHIA COCOA REVIEW</title><DATELINE>    SALVADOR, Feb 26 - </DATELINE><body>Showers continued throughout the week inthe Bahia cocoa zone, alleviating the drought since earlyJanuary and improving prospects for the coming temporao,although normal humidity levels have not been restored,Comissaria Smith said in its weekly review.";
+a = Parser.ReutersParser(s);
 
-// System.Console.WriteLine(a.TitleInit);
-// System.Console.WriteLine(a.TitleLen);
-// System.Console.WriteLine(a.TextLen);
-// System.Console.WriteLine(a.SnippetLen);
+System.Console.WriteLine("-----Routers-----");
+System.Console.WriteLine($"Title init: {a.TitleInit}");
+System.Console.WriteLine($"Title Len: {a.TitleLen}");
+System.Console.WriteLine($"Text Init: {a.TextInit}");
+System.Console.WriteLine($"Snippet init: {a.SnippetInit}");
+
+s= ".I 1.t\nexperimental investigation of the aerodynamics of awing in a slipstream .\n.a\nbrenckman,m..Bj. ae. scs. 25, 1958, 324. .w\nexperimental investigation of the aerodynamics of a wing in a slipstream . an experimental study of a wing in a propeller slipstream was made in order to determine the spanwise distribution of the lift increase due to slipstream at different angles of attack of the wing.";
+a = Parser.CranParser(s);
+
+System.Console.WriteLine("-----Cran-----");
+System.Console.WriteLine($"Title init: {a.TitleInit}");
+System.Console.WriteLine($"Title Len: {a.TitleLen}");
+System.Console.WriteLine($"Text Init: {a.TextInit}");
+System.Console.WriteLine($"Snippet init: {a.SnippetInit}");
